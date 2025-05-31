a44272a1bf1815cc0eb6bd332ecc08da3749beeb
codewithaditya07 <adityasahani680@gmail.com>
2025-05-31 23:34:23 +0530

    C

--- Examm.c
+++ Examm.c
@@ -1,17 +1,36 @@
 // 1. Factorial Using Recursion
+// #include <stdio.h>
+
+// int factorial(int n) {
+//     if (n <= 1)
+//         return 1;
+//     else
+//         return n * factorial(n - 1);
+// }
+
+// int main() {
+//     int n;
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+//     printf("Factorial of %d is %d\n", n, factorial(n));
+//     return 0;
+// }
+
+// 2. String Length Without Using strlen()
 #include <stdio.h>
 
-int factorial(int n) {
-    if (n <= 1)
-        return 1;
-    else
-        return n * factorial(n - 1);
+int stringLength(char str[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        i++;
+    }
+    return i;
 }
 
 int main() {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    printf("Factorial of %d is %d\n", n, factorial(n));
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("Length of the string is %d\n", stringLength(str));
     return 0;
 }
