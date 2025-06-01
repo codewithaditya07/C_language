f19026451bc17db08f8d905bae2fb0b2454dc290
codewithaditya07 <adityasahani680@gmail.com>
2025-06-01 23:29:07 +0530

    C

--- Examm.c
+++ Examm.c
@@ -17,20 +17,46 @@
 // }
 
 // 2. String Length Without Using strlen()
-#include <stdio.h>
+// #include <stdio.h>
 
-int stringLength(char str[]) {
-    int i = 0;
-    while (str[i] != '\0') {
-        i++;
+// int stringLength(char str[]) {
+//     int i = 0;
+//     while (str[i] != '\0') {
+//         i++;
+//     }
+//     return i;
+// }
+
+// int main() {
+//     char str[100];
+//     printf("Enter a string: ");
+//     scanf("%s", str);
+//     printf("Length of the string is %d\n", stringLength(str));
+//     return 0;
+// }
+
+// 3. Check Palindrome Using Pointers
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char *str) {
+    char *end = str + strlen(str) - 1;
+    while (str < end) {
+        if (*str != *end)
+            return 0;
+        str++;
+        end--;
     }
-    return i;
+    return 1;
 }
 
 int main() {
     char str[100];
     printf("Enter a string: ");
     scanf("%s", str);
-    printf("Length of the string is %d\n", stringLength(str));
+    if (isPalindrome(str))
+        printf("Palindrome\n");
+    else
+        printf("Not Palindrome\n");
     return 0;
 }
