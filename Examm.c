4510152518127f8817d5eb341889ee4711a7c60d
codewithaditya07 <adityasahani680@gmail.com>
2025-06-02 20:37:22 +0530

    C

--- Examm.c
+++ Examm.c
@@ -36,27 +36,62 @@
 // }
 
 // 3. Check Palindrome Using Pointers
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int isPalindrome(char *str) {
-    char *end = str + strlen(str) - 1;
-    while (str < end) {
-        if (*str != *end)
-            return 0;
-        str++;
-        end--;
-    }
-    return 1;
-}
+// int isPalindrome(char *str) {
+//     char *end = str + strlen(str) - 1;
+//     while (str < end) {
+//         if (*str != *end)
+//             return 0;
+//         str++;
+//         end--;
+//     }
+//     return 1;
+// }
 
-int main() {
-    char str[100];
-    printf("Enter a string: ");
-    scanf("%s", str);
-    if (isPalindrome(str))
-        printf("Palindrome\n");
-    else
-        printf("Not Palindrome\n");
-    return 0;
-}
+// int main() {
+//     char str[100];
+//     printf("Enter a string: ");
+//     scanf("%s", str);
+//     if (isPalindrome(str))
+//         printf("Palindrome\n");
+//     else
+//         printf("Not Palindrome\n");
+//     return 0;
+// }
+
+
+// 4. Copy Contents from One File to Another
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main() {
+//     FILE *source, *dest;
+//     char ch, sourceName[100], destName[100];
+
+//     printf("Enter source file name: ");
+//     scanf("%s", sourceName);
+//     source = fopen(sourceName, "r");
+//     if (source == NULL) {
+//         printf("Cannot open source file.\n");
+//         return 1;
+//     }
+
+//     printf("Enter destination file name: ");
+//     scanf("%s", destName);
+//     dest = fopen(destName, "w");
+//     if (dest == NULL) {
+//         printf("Cannot open destination file.\n");
+//         fclose(source);
+//         return 1;
+//     }
+
+//     while ((ch = fgetc(source)) != EOF)
+//         fputc(ch, dest);
+
+//     printf("File copied successfully.\n");
+//     fclose(source);
+//     fclose(dest);
+//     return 0;
+// }
