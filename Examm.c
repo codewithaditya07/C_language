3572ea02cd86d2e05bf48bacf42eb4e1ef1495ca
codewithaditya07 <adityasahani680@gmail.com>
2025-06-03 23:41:19 +0530

    C

--- Examm.c
+++ Examm.c
@@ -95,3 +95,37 @@
 //     fclose(dest);
 //     return 0;
 // }
+
+// 5. Matrix Multiplication (3x3)
+#include <stdio.h>
+
+int main() {
+    int A[3][3], B[3][3], C[3][3], i, j, k;
+
+    printf("Enter elements of Matrix A (3x3):\n");
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Enter elements of Matrix B (3x3):\n");
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            scanf("%d", &B[i][j]);
+
+    // Multiply A * B
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < 3; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+
+    printf("Resultant Matrix C:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++)
+            printf("%d ", C[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
