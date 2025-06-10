14972c2594554e1024e273f34740f033c88db7b0
codewithaditya07 <adityasahani680@gmail.com>
2025-06-10 23:57:07 +0530

    C

--- Examm.c
+++ Examm.c
@@ -230,25 +230,37 @@
 // }
 
 // Q4. (10 Marks) Multiply two 3x3 matrices.
+// #include <stdio.h>
+// int main() {
+//     int a[3][3], b[3][3], c[3][3], i, j, k;
+//     printf("Enter Matrix A:\n");
+//     for (i = 0; i < 3; i++) for (j = 0; j < 3; j++) scanf("%d", &a[i][j]);
+//     printf("Enter Matrix B:\n");
+//     for (i = 0; i < 3; i++) for (j = 0; j < 3; j++) scanf("%d", &b[i][j]);
+
+//     for (i = 0; i < 3; i++)
+//         for (j = 0; j < 3; j++) {
+//             c[i][j] = 0;
+//             for (k = 0; k < 3; k++)
+//                 c[i][j] += a[i][k] * b[k][j];
+//         }
+
+//     printf("Result:\n");
+//     for (i = 0; i < 3; i++) {
+//         for (j = 0; j < 3; j++) printf("%d ", c[i][j]);
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// Q5. (10 Marks) Find string length without using strlen().
 #include <stdio.h>
 int main() {
-    int a[3][3], b[3][3], c[3][3], i, j, k;
-    printf("Enter Matrix A:\n");
-    for (i = 0; i < 3; i++) for (j = 0; j < 3; j++) scanf("%d", &a[i][j]);
-    printf("Enter Matrix B:\n");
-    for (i = 0; i < 3; i++) for (j = 0; j < 3; j++) scanf("%d", &b[i][j]);
-
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++) {
-            c[i][j] = 0;
-            for (k = 0; k < 3; k++)
-                c[i][j] += a[i][k] * b[k][j];
-        }
-
-    printf("Result:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) printf("%d ", c[i][j]);
-        printf("\n");
-    }
+    char str[100];
+    int len = 0;
+    printf("Enter string: ");
+    scanf("%s", str);
+    while (str[len] != '\0') len++;
+    printf("Length = %d\n", len);
     return 0;
 }
