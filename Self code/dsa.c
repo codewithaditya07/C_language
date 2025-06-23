3fcb54a9cecf4cd8b8d34e71a78c822c539a62ea
codewithaditya07 <adityasahani680@gmail.com>
2025-06-23 18:01:34 +0530

    C

--- Self code/dsa.c
+++ Self code/dsa.c
@@ -28,36 +28,67 @@
 // }
 
 // 2. Binary Search in Array
+// #include <stdio.h>
+
+// int main() {
+//     int arr[100], n, i, key, low, high, mid;
+
+//     printf("Enter number of elements: ");
+//     scanf("%d", &n);
+
+//     printf("Enter sorted array elements: ");
+//     for (i = 0; i < n; i++)
+//         scanf("%d", &arr[i]);
+
+//     printf("Enter element to search: ");
+//     scanf("%d", &key);
+
+//     low = 0;
+//     high = n - 1;
+
+//     while (low <= high) {
+//         mid = (low + high) / 2;
+
+//         if (arr[mid] == key) {
+//             printf("Element found at position %d\n", mid + 1);
+//             return 0;
+//         } else if (arr[mid] < key)
+//             low = mid + 1;
+//         else
+//             high = mid - 1;
+//     }
+
+//     printf("Element not found.\n");
+//     return 0;
+// }
+
+// 3. Bubble Sort
 #include <stdio.h>
 
 int main() {
-    int arr[100], n, i, key, low, high, mid;
+    int arr[100], n, i, j, temp;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter sorted array elements: ");
+    printf("Enter array elements: ");
     for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    printf("Enter element to search: ");
-    scanf("%d", &key);
-
-    low = 0;
-    high = n - 1;
-
-    while (low <= high) {
-        mid = (low + high) / 2;
-
-        if (arr[mid] == key) {
-            printf("Element found at position %d\n", mid + 1);
-            return 0;
-        } else if (arr[mid] < key)
-            low = mid + 1;
-        else
-            high = mid - 1;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
 
-    printf("Element not found.\n");
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
     return 0;
 }
