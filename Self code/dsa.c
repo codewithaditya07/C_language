fc3d732d9d9c10f758f8d4e9d4f3dee2b40ea8c7
codewithaditya07 <adityasahani680@gmail.com>
2025-06-25 14:48:57 +0530

    C

--- Self code/dsa.c
+++ Self code/dsa.c
@@ -92,3 +92,45 @@
 
 //     return 0;
 // }
+
+// 4. Stack Implementation using Array
+#include <stdio.h>
+#define MAX 5
+
+int stack[MAX], top = -1;
+
+void push(int val) {
+    if (top == MAX - 1)
+        printf("Stack Overflow\n");
+    else
+        stack[++top] = val;
+}
+
+void pop() {
+    if (top == -1)
+        printf("Stack Underflow\n");
+    else
+        printf("Popped element: %d\n", stack[top--]);
+}
+
+void display() {
+    int i;
+    if (top == -1)
+        printf("Stack is empty\n");
+    else {
+        printf("Stack elements: ");
+        for (i = 0; i <= top; i++)
+            printf("%d ", stack[i]);
+        printf("\n");
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
+    return 0;
+}
