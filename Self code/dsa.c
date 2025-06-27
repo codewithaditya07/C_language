02d63213eefb2925560ee96142923bd657106e31
codewithaditya07 <adityasahani680@gmail.com>
2025-06-27 23:16:20 +0530

    C

--- Self code/dsa.c
+++ Self code/dsa.c
@@ -137,45 +137,45 @@
 
 
 // 5. Queue Implementation using Array
-#include <stdio.h>
-#define MAX 5
+// #include <stdio.h>
+// #define MAX 5
 
-int queue[MAX], front = -1, rear = -1;
+// int queue[MAX], front = -1, rear = -1;
 
-void enqueue(int val) {
-    if (rear == MAX - 1)
-        printf("Queue Overflow\n");
-    else {
-        if (front == -1) front = 0;
-        queue[++rear] = val;
-    }
-}
+// void enqueue(int val) {
+//     if (rear == MAX - 1)
+//         printf("Queue Overflow\n");
+//     else {
+//         if (front == -1) front = 0;
+//         queue[++rear] = val;
+//     }
+// }
 
-void dequeue() {
-    if (front == -1 || front > rear)
-        printf("Queue Underflow\n");
-    else
-        printf("Dequeued element: %d\n", queue[front++]);
-}
+// void dequeue() {
+//     if (front == -1 || front > rear)
+//         printf("Queue Underflow\n");
+//     else
+//         printf("Dequeued element: %d\n", queue[front++]);
+// }
 
-void display() {
-    int i;
-    if (front == -1 || front > rear)
-        printf("Queue is empty\n");
-    else {
-        printf("Queue elements: ");
-        for (i = front; i <= rear; i++)
-            printf("%d ", queue[i]);
-        printf("\n");
-    }
-}
+// void display() {
+//     int i;
+//     if (front == -1 || front > rear)
+//         printf("Queue is empty\n");
+//     else {
+//         printf("Queue elements: ");
+//         for (i = front; i <= rear; i++)
+//             printf("%d ", queue[i]);
+//         printf("\n");
+//     }
+// }
 
-int main() {
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
-    display();
-    dequeue();
-    display();
-    return 0;
-}
+// int main() {
+//     enqueue(10);
+//     enqueue(20);
+//     enqueue(30);
+//     display();
+//     dequeue();
+//     display();
+//     return 0;
+// }
