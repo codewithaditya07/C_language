034014a89dfb5b3cb4e07e0ede0797732aee01d4
codewithaditya07 <adityasahani680@gmail.com>
2025-06-30 23:02:15 +0530

    C

--- Self code/dsa.c
+++ Self code/dsa.c
@@ -170,12 +170,12 @@
 //     }
 // }
 
-// int main() {
-//     enqueue(10);
-//     enqueue(20);
-//     enqueue(30);
-//     display();
-//     dequeue();
-//     display();
-//     return 0;
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
+    return 0;
 // }
