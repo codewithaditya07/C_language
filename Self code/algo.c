78a76128350cfb2b16071703e9dcded515e946c7
codewithaditya07 <adityasahani680@gmail.com>
2025-07-21 22:12:19 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -126,3 +126,28 @@ int main() {
     return 0;
 }
 
+// 7. Matrix Multiplication (3x3)
+#include <stdio.h>
+
+int main() {
+    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int b[3][3] = {{9,8,7},{6,5,4},{3,2,1}};
+    int c[3][3], i, j, k;
+
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++) {
+            c[i][j] = 0;
+            for (k = 0; k < 3; k++)
+                c[i][j] += a[i][k] * b[k][j];
+        }
+
+    printf("Resultant Matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++)
+            printf("%d ", c[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
+
