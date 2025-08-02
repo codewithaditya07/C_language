cbe50f35f12e96e77e3950b5fad563a777f94748
codewithaditya07 <adityasahani680@gmail.com>
2025-08-02 21:55:06 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -201,3 +201,19 @@ int main() {
     printf("Length of the string is: %d\n", length);
     return 0;
 }
+
+// 11. Sum and Average of Array
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int sum = 0, i;
+    float avg;
+
+    for (i = 0; i < 6; i++)
+        sum += arr[i];
+
+    avg = (float)sum / 6;
+    printf("Sum = %d\nAverage = %.2f\n", sum, avg);
+    return 0;
+}
