35ffcb40d003412227f76aa23c6073d564e21562
codewithaditya07 <adityasahani680@gmail.com>
2025-08-04 15:52:43 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -235,3 +235,26 @@ int main() {
     printf("Copied string: %s\n", dest);
     return 0;
 }
+
+// 13. Palindrome Check
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100] = "madam";
+    int i, len = strlen(str), flag = 1;
+
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
