3b249c21ef02f014fd811fe86184abe113f164cc
codewithaditya07 <adityasahani680@gmail.com>
2025-08-05 17:55:06 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -258,3 +258,18 @@ int main() {
 
     return 0;
 }
+
+// 14. Exponentiation (x^n)
+#include <stdio.h>
+
+int main() {
+    int x = 2, n = 5, result = 1;
+
+    while (n > 0) {
+        result *= x;
+        n--;
+    }
+
+    printf("Result = %d\n", result);
+    return 0;
+}
