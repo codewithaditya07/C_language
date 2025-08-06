20cb302d9950c8616b5b593ead351d3c3f5f1313
codewithaditya07 <adityasahani680@gmail.com>
2025-08-06 22:04:58 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -273,3 +273,19 @@ int main() {
     printf("Result = %d\n", result);
     return 0;
 }
+
+// 15. Count Occurrence of a Numbe
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 10, 15, 2, 10, 8, 7, 10, 11};
+    int count = 0, i;
+
+    for (i = 0; i < 9; i++) {
+        if (arr[i] == 10)
+            count++;
+    }
+
+    printf("10 occurs %d times.\n", count);
+    return 0;
+}
