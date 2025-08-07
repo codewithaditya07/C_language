95900b6f7cb12b793c09689b1db8bc1521046fb8
codewithaditya07 <adityasahani680@gmail.com>
2025-08-07 23:06:17 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -289,3 +289,17 @@ int main() {
     printf("10 occurs %d times.\n", count);
     return 0;
 }
+// 16. Find Location of First Odd Number
+#include <stdio.h>
+
+int main() {
+    int arr[] = {2, 4, 6, 9, 10}, i;
+
+    for (i = 0; i < 5; i++) {
+        if (arr[i] % 2 != 0) {
+            printf("First odd number %d at index %d\n", arr[i], i);
+            break;
+        }
+    }
+    return 0;
+}
