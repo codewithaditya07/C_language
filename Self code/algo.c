85f5ceb5d41689821f09ead1b841eeb3e02cc705
codewithaditya07 <adityasahani680@gmail.com>
2025-09-06 22:33:45 +0530

    C

--- Self code/algo.c
+++ Self code/algo.c
@@ -292,14 +292,14 @@
 // 16. Find Location of First Odd Number
 // #include <stdio.h>
 
-int main() {
-    int arr[] = {2, 4, 6, 9, 10}, i;
+// int main() {
+//     int arr[] = {2, 4, 6, 9, 10}, i;
 
-    for (i = 0; i < 5; i++) {
-        if (arr[i] % 2 != 0) {
-            printf("First odd number %d at index %d\n", arr[i], i);
-            break;
-        }
-     }
-    return 0;
-}
+//     for (i = 0; i < 5; i++) {
+//         if (arr[i] % 2 != 0) {
+//             printf("First odd number %d at index %d\n", arr[i], i);
+//             break;
+//         }
+//      }
+//     return 0;
+// }
