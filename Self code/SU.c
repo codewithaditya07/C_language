5944e6d96f9da556fdfffd4835929e67163dddfb
codewithaditya07 <adityasahani680@gmail.com>
2025-09-19 23:24:45 +0530

    C

--- Self code/SU.c
+++ Self code/SU.c
@@ -1,25 +1,25 @@
 // Basic C Program for Fibonacci Series
-#include <stdio.h>
-int main() {
-int n, i;
-int first = 0, second = 1, next;
-printf("Enter the number of terms: ");
-scanf("%d", &n);
-printf("Fibonacci Series: ");
-for (i = 1; i <= n; i++) {
-if (i == 1) {
-printf("%d ", first);
-continue;
-}
-if (i == 2) {
-printf("%d ", second);
-continue;
-}
-next = first + second;
-first = second;
-second = next;
-printf("%d ", next);
-}
-printf("\n");
-return 0;
-}
+// #include <stdio.h>
+// int main() {
+// int n, i;
+// int first = 0, second = 1, next;
+// printf("Enter the number of terms: ");
+// scanf("%d", &n);
+// printf("Fibonacci Series: ");
+// for (i = 1; i <= n; i++) {
+// if (i == 1) {
+// printf("%d ", first);
+// continue;
+// }
+// if (i == 2) {
+// printf("%d ", second);
+// continue;
+// }
+// next = first + second;
+// first = second;
+// second = next;
+// printf("%d ", next);
+// }
+// printf("\n");
+// return 0;
+// }
